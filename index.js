1b6988d7cc262ec5d5c4cdbf3926aa30b8cb227d
Omar Jajah <omar.jajah@key.sa>
2021-03-10 02:14:38 +0300

    CORS added

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const timestamp = require('./timestamp');
-
+const cors = require('cors');
 const app = express();
 
 const HOSTNAME = process.env.HOSTNAME || 'localhost';
 const PORT = process.env.PORT || 3000;
 
+app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(express.static('public'));
 
 app.get('/api/timestamp/:date?', (req, res) => {
